1d59c5cfad08d1ffdf9fcf8678d6cdaf543a25fd
[scsawe@gmail.com] <100406928+sarahsawe@users.noreply.github.com>
2023-06-13 09:16:56 +0300

    main template

---
+++ beneficiary_managemen/manage.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
+
+
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'beneficiary_managemen.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
+    
+from django.db import models
+
+class Beneficiary(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+    # Add more fields as per your requirements
+
+class CashTransfer(models.Model):
+    beneficiary = models.ForeignKey(Beneficiary, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField(auto_now_add=True)
+    # Add more fields as per your requirements
